87b6f1177ba32e9000dd4fb335c7eca56389c064
Marissa <marissacurry809@gmail.com>
2025-04-24 19:05:30 -0700

    simple calculator

---
+++ hellokittycal/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data;
+
+namespace MyCalculator 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1=0;
+            double num2=0;
+            double result =0;
+
+
+            Console.WriteLine("Calculator Program");
+
+            Console.WriteLine("Enter number 1");
+            num1= Convert.ToDouble(Console.ReadLine());
+             
+            Console.WriteLine("Enter number 1");
+            num2= Convert.ToDouble(Console.ReadLine());
+             
+
+             Console.WriteLine("Enter an option");
+
+             Console.WriteLine("+ : Add ");
+
+             Console.WriteLine("  - : Subtract");
+
+             Console.WriteLine(" * : Multiply");
+
+             Console.WriteLine("/ : Divide ");
+
+             switch (Console.ReadLine())
+             {
+                 case "+":
+                 result= num1+num2;
+                 Console.WriteLine($"Your result is:   {num1}+ {num2} = " +result );
+                 break;
+
+                 case "-":
+                 result= num1+num2;
+                 Console.WriteLine($"Your result is:   {num1}+ {num2} = " +result );
+                 break;
+
+                 case " * ":
+                 result= num1+num2;
+                 Console.WriteLine($"Your result is:   {num1}+ {num2} = " +result );
+                 break;
+
+                 case "/":
+                 result= num1+num2;
+                 Console.WriteLine($"Your result is:   {num1}+ {num2} = " +result );
+                 break;
+
+                default:
+                 Console.WriteLine("Your result is invalid");
+                break;
+
+             }
+            Console.ReadKey();
+        }
+
+    }
+}
